293e07cf332de314a83134e7e4b4fb4331a971b1
AnatoliyLevchenko <lewanatolij@gmail.com>
2018-04-24 23:19:20 +0300

    create unit tests

---
+++ WebApplication.Tests/UnitTest1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApplication.Models;
+
+namespace WebApplication.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private IDiscounteHelper getTestObject()
+        {
+            return new MinimumDiscountHelper();
+        }
+        [TestMethod]
+        public void DiscountAbove100()
+        {
+            //arrange
+            var target = getTestObject();
+            const decimal total = 200m;
+            //act
+            var discountedTotal = target.ApplyDiscount(total);
+            //assert
+            Assert.AreEqual(total * 0.9m, discountedTotal);
+        }
+
+        [TestMethod]
+        public void DiscountBetween10And100()
+        {
+            //arrange
+            var target = getTestObject();
+            //act
+            var tenDollarDiscount = target.ApplyDiscount(10);
+            var hudredDollarDiscount = target.ApplyDiscount(100);
+            var fiftyDollarDiscount = target.ApplyDiscount(50);
+            //assert
+            Assert.AreEqual(5, tenDollarDiscount, "10$ discount is wrong");
+            Assert.AreEqual(95, hudredDollarDiscount, "100$ discount is wrong");
+            Assert.AreEqual(45, fiftyDollarDiscount, "50$ discount is wrong");
+        }
+        [TestMethod]
+        public void DiscountLess10()
+        {
+            //arrange
+            var target = getTestObject();
+            //act
+            var discount5 = target.ApplyDiscount(5);
+            var discount0 = target.ApplyDiscount(0);
+
+            //assert
+            Assert.AreEqual(5, discount5);
+            Assert.AreEqual(0, discount0);
+        }
+        [TestMethod]
+        [ExpectedException((typeof(ArgumentOutOfRangeException)))]
+        public void DiscountNegativeTotal()
+        {
+            //arrange
+            var target = getTestObject();
+            //act
+            target.ApplyDiscount(-1);
+        }
+    }
+}
---
+++ WebApplication.Tests/UnitTest2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApplication.Models;
+using Moq;
+
+namespace WebApplication.Tests
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        private readonly Product[] _products =
+        {
+            new Product() {Name = "Kayak", Category = "Watersports", Price = 275m},
+            new Product() {Name = "Lifejacket", Category = "Watersports", Price = 49.95m},
+            new Product() {Name = "Soccer ball", Category = "Soccer", Price = 19.50m},
+            new Product() {Name = "Corner flag", Category = "Soccer", Price = 34.95m}
+        };
+        [TestMethod]
+        public void Sum_Products_Correctly()
+        {
+            //arrange
+            var mock = new Mock<IDiscounteHelper>();
+            mock.Setup(m => m.ApplyDiscount(It.IsAny<decimal>())).Returns<decimal>(total => total);
+            var target = new LinqValueCalculator(mock.Object);
+            //act
+            var result = target.ValueProducts(_products);
+            //assert
+            Assert.AreEqual(_products.Sum(e => e.Price), result);
+        }
+    }
+}
